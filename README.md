826427157afc55b1148757ffffee6b3f89b1459c
johnprattchristian <johnprattchristian@gmail.com>
2016-12-31 12:15:02 -0500

    Merge remote-tracking branch 'refs/remotes/origin/master' into gh-pages
    # Conflicts:
    #	index.html

--- README.md
+++
@@ -1,2 +0,0 @@
-# imagecollection
-Collect your favorite images online
--- help.js
+++
@@ -1,9 +0,0 @@
-// Retrieve a setting object by 'item' property (the name) from the _settings arrays for use
-var getTipFromArray = function(tip){
-	// retrieve the first item from an array of settings that match the given name
-	return $.grep(helpTips,function(e){return e.item === tip})[0];
-};
-
-$(function(){
-	
-});
